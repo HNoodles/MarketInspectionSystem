349733b45d8a8cbffd7743749ea101ec3ee8d348
HNoodles <13671980486@163.com>
2021-01-10 04:01:02 +0800

    added MarketGradingVisitor

--- src/main/java/com/fudan/market_inspection/service/visitor/AbstractVisitor.java
+++ src/main/java/com/fudan/market_inspection/service/visitor/AbstractVisitor.java
@@ -3,5 +3,5 @@ package com.fudan.market_inspection.service.visitor;
 import com.fudan.market_inspection.dao.CheckTask;
 
 public abstract class AbstractVisitor {
-    abstract public <T> void visit(CheckTask checkTask);
+    abstract public void visit(CheckTask checkTask);
 }
--- src/main/java/com/fudan/market_inspection/service/visitor/MarketGradingVisitor.java
+++ src/main/java/com/fudan/market_inspection/service/visitor/MarketGradingVisitor.java
@@ -1,19 +1,20 @@
 package com.fudan.market_inspection.service.visitor;
 
 import com.fudan.market_inspection.dao.CheckTask;
-import com.fudan.market_inspection.entity.Product;
+import com.fudan.market_inspection.dao.GradeInfo;
+import com.fudan.market_inspection.entity.Market;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class CheckInvalidVisitor extends AbstractVisitor {
-    private final Map<Product, Integer> result;
+public class MarketGradingVisitor extends AbstractVisitor {
+    private final Map<Market, GradeInfo> result;
 
-    public CheckInvalidVisitor() {
+    public MarketGradingVisitor() {
         result = new HashMap<>();
     }
 
-    public Map<Product, Integer> getResult() {
+    public Map<Market, GradeInfo> getResult() {
         return result;
     }
 
